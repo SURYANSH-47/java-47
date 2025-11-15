80e25e82ab4ab2284c1fffaeedf2a3dc73f0c5a9
SURYANSH-47 <suryanshjha.cs24@bmsce.ac.in>
2025-11-15 23:27:47 +0530

    Delete Lab6 directory

--- Lab6/Asamp.java
+++
@@ -1,7 +0,0 @@
-import Sample.Samp;
-public class Asamp{
-    public static void main(String[] args) {
-        Samp a=new Samp();
-        a.show();
-    }
-}
--- Lab6/Lab6.java
+++
@@ -1,9 +0,0 @@
-import SEE.Externals;
-class Lab6{
-    public static void main(String[] args) {
-        Externals we=new Externals();
-        we.input_details();we.inputCIEmarks();we.inputSEEmarks();we.calculateFinalMarks();
-        we.display_marks();
-
-    }
-}
