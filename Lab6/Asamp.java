6cce6110a092ebdc8950505ece4a3a7064e63515
SURYANSH-47 <suryanshjha.cs24@bmsce.ac.in>
2025-11-15 23:26:40 +0530

    Add files via upload

---
+++ Lab6/Asamp.java
@@ -0,0 +1,7 @@
+import Sample.Samp;
+public class Asamp{
+    public static void main(String[] args) {
+        Samp a=new Samp();
+        a.show();
+    }
+}
---
+++ Lab6/Lab6.java
@@ -0,0 +1,9 @@
+import SEE.Externals;
+class Lab6{
+    public static void main(String[] args) {
+        Externals we=new Externals();
+        we.input_details();we.inputCIEmarks();we.inputSEEmarks();we.calculateFinalMarks();
+        we.display_marks();
+
+    }
+}
